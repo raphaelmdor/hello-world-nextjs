7f2f383a0a67bcbf6ee2851885eab7fb7654b927
Raphael Monteiro de Oliveira Rebello <raphaelsiqueiraduarte@gmail.com>
2023-09-29 10:22:44 -0300

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,7 +10,7 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   console.log(req.method);
-  res.status(200).json({name: ""});
+  res.status(200).json({name: "Raphael Monteiro"});
 }
 
 
